2ebcdd389445b066d4ed83811bee8727c1ef65e8
ukitaka <yuki.takahashi.1126@gmail.com>
2017-10-04 17:08:07 +0900

    Add tests

--- Tests/SystemFTests/TypeTests.swift
+++ Tests/SystemFTests/TypeTests.swift
@@ -4,6 +4,7 @@ import XCTest
 class TypeTests: XCTestCase {
     func testType() {
         XCTAssertEqual(Type.typeVar("X"), Type.typeVar("X"))
+        XCTAssertNotEqual(Type.typeVar("X"), Type.typeVar("Y"))
         XCTAssertEqual(Type.int, Type.int)
         XCTAssertEqual(Type.bool, Type.bool)
     }
