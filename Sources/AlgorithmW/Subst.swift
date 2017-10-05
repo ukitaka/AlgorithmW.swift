16cca81f738a98013d903aa361b4e7344e8083ac
ukitaka <yuki.takahashi.1126@gmail.com>
2017-10-05 23:06:11 +0900

    Impl Subst

--- Sources/AlgorithmW/Subst.swift
+++ Sources/AlgorithmW/Subst.swift
@@ -2,6 +2,18 @@
 // Created by ukitaka on 2017/10/04.
 //
 
-class Subst {
-    let map: [String: Type] = [:]
+struct Subst {
+    let map: [String: Type]
+
+    init() {
+        self.map = [:]
+    }
+
+    init(map: [String: Type]) {
+        self.map = map
+    }
+
+    func compose(other: Subst) -> Subst {
+        return Subst(map: self.map.merging(other.map, uniquingKeysWith: { _, t2 in t2 }))
+    }
 }
