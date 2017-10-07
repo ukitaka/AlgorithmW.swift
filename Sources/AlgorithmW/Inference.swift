5853153fcc4fa1d3edddc70b52f23efa1188b94b
ukitaka <yuki.takahashi.1126@gmail.com>
2017-10-07 23:36:41 +0900

    Fix

--- Sources/AlgorithmW/Inference.swift
+++ Sources/AlgorithmW/Inference.swift
@@ -12,7 +12,7 @@ struct Inference {
         }
     }
 
-    static func typeInference(env: TypeEnvironment, term: Term) -> (Substitution, Type) {
+    static func typeInferenceTerm(env: TypeEnvironment, term: Term) -> (Substitution, Type) {
         switch term {
         case let .variable(variable):
             if let scheme = env[variable] {
@@ -26,21 +26,26 @@ struct Inference {
             let tv = Type.typeVar(TypeVariable())
             let env2 = env.removing(variable: variable)
             let env3 = env2.union(other: TypeEnvironment([variable: TypeScheme(variables: [], type: tv)]))
-            let (s1, t1) = typeInference(env: env3, term: term2)
+            let (s1, t1) = typeInferenceTerm(env: env3, term: term2)
             return (s1, .function(tv.apply(s1), t1))
         case let .application(termL, termR):
             let tv = Type.typeVar(TypeVariable())
-            let (s1, t1) = typeInference(env: env, term: termL)
-            let (s2, t2) = typeInference(env: env.apply(s1), term: termR)
+            let (s1, t1) = typeInferenceTerm(env: env, term: termL)
+            let (s2, t2) = typeInferenceTerm(env: env.apply(s1), term: termR)
             let s3 = Unification.mgu(t1.apply(s2), .function(t2, tv))
             return (s3.compose(other: s2).compose(other: s1), tv.apply(s3))
         case let .let(variable, termBind, termBody):
-            let (s1, t1) = typeInference(env: env, term: termBind)
+            let (s1, t1) = typeInferenceTerm(env: env, term: termBind)
             let env2 = env.removing(variable: variable)
             let td = t1.generalize(env)
             let env3 = env2.inserting(variable: variable, scheme: td)
-            let (s2, t2) = typeInference(env: env3.apply(s1), term: termBody)
+            let (s2, t2) = typeInferenceTerm(env: env3.apply(s1), term: termBody)
             return (s1.compose(other: s2), t2)
         }
+    }
+
+    static func typeInference(env: TypeEnvironment, term: Term) -> Type {
+        let (s, t) = typeInferenceTerm(env: env, term: term)
+        return t.apply(s)
     }
 }
