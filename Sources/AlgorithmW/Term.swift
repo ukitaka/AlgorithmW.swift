ab003448637dca3e8debca1d4bb692b7500d33f9
ukitaka <yuki.takahashi.1126@gmail.com>
2017-10-08 15:13:35 +0900

    Impl `CustromStringConvertible`

--- Sources/AlgorithmW/Term.swift
+++ Sources/AlgorithmW/Term.swift
@@ -9,3 +9,22 @@ indirect enum Term {
     case abstraction(TypeVariable, Term)
     case `let`(TypeVariable, Term, Term)
 }
+
+extension Term: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case let .variable(v):
+            return v.name
+        case let .literal(.integer(i)):
+            return "\(i)"
+        case let .literal(.boolean(b)):
+            return "\(b)"
+        case let .application(term1, term2):
+             return "(\(term1) \(term2))"
+        case let .abstraction(variable, term):
+            return "λ \(variable.name).\(term)"
+        case let .let(variable, bind, body):
+            return "let \(variable.name) = \(bind) in \(body)"
+        }
+    }
+}
